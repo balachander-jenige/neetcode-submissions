df6e9232f3769040040187d7faf2ecb11e7859f0
Balachander Jenige <eduwithbala@gmail.com>
2026-07-08 11:16:38 +0800

    Add: is-anagram - submission-3

---
+++ Data Structures & Algorithms/is-anagram/submission-3.cpp
@@ -0,0 +1,18 @@
+class Solution {
+public:
+    bool isAnagram(string s, string t) {
+        unordered_map<char, int> ana;
+        for (int i=0;i<s.size();i++){
+            ana[s[i]] ++;
+        }
+        for (int i=0;i<t.size();i++){
+            ana[t[i]] --;
+            if(ana[t[i]]<0)
+            {
+                return false;
+            }
+        }
+        return true;
+
+    }
+};
